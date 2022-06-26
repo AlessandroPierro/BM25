b60db3c8d22c237a89d9b69f026efb7aa9b0ebce
Alessandro Pierro <40338642+AlessandroPierro@users.noreply.github.com>
2022-06-26 15:26:49 +0200

    Various improvements

--- bm25.py
+++ bm25.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 import re
 import math
 import pandas as pd
@@ -10,58 +11,55 @@ STOP_WORDS = set(stopwords.words('english'))
 
 class BM25(object):
     """
-    Class to implement bm25 ranking algorithm, given a corpus of documents.
+    Class to implement BM25 information retrieval algorithm.
     """
 
-    def __init__(self, corpus: pd.DataFrame, k: float, b: float, delta: float, rf_docs=None, rf_terms=10) -> None:
+    def __init__(self, corpus: pd.DataFrame, k1: float, b: float, delta: float, rf_docs: int, rf_terms: int) -> None:
         """
-        bm25 class initializer.
+        BM25 class initializer.
 
         params:
             corpus: Pandas DataFrame with a column "text"
-            k (float): parameter controlling the impact of the term frequency
-
+            k1 (float): parameter controlling the impact of the term frequency
+            b (float): parameter controlling the impact of the document length
+            delta (float): normalizing parameter introduced in the BM25+ variant
+            rf_docs (int): number of documents to use for pseudo-relevance feedback
+            rf_terms (int): number of terms to use for pseudo-relevance feedback
         """
-        self.corpus = corpus
-        self.k = k
+        self._corpus = corpus
+        self.k1 = k1
         self.b = b
         self.delta = delta
         self.rf_docs = rf_docs
         self.rf_terms = rf_terms
 
-        self.corpus["parsed_text"] = self.corpus["text"].apply(
-            self.preprocess_text)
-        self.corpus["word_count"] = self.corpus["parsed_text"].apply(
+        self._corpus["parsed_text"] = self._corpus["text"].apply(
+            self._preprocess_text)
+        self._corpus["word_count"] = self._corpus["parsed_text"].apply(
             lambda text: len(text.split(" ")))
-        self.avg_word_count = self.corpus["word_count"].mean()
+        self._avg_word_count = self._corpus["word_count"].mean()
 
-        self.vocabulary = BM25.build_vocabulary(corpus)
-        self.tf = self.compute_tf()
-        self.idf = BM25.compute_idf(self.tf, self.vocabulary, len(self.corpus))
-        self.corpus.drop(columns=["parsed_text", "text"], inplace=True)
+        self._vocabulary = self._build_vocabulary()
+        self._tf = self._compute_tf()
+        self._idf = self._compute_idf()
+        self._corpus.drop(columns=["parsed_text", "text"], inplace=True)
 
-    @staticmethod
-    def build_vocabulary(df: pd.DataFrame) -> Set[str]:
+    def _build_vocabulary(self) -> Set[str]:
         """
-        Given a Pandas DataFrame with a column "parsed_text", returns a set of all unique words in the corpus.
-
-        params:
-            df: Pandas DataFrame with a column "parsed_text"
+        Returns a set of all unique words in the corpus, based on the 'parsed_text' column of self.corpus.
 
         returns:
             vocabulary (set): set of all unique words in the corpus
         """
         vocabulary = set()
-        df["parsed_text"].apply(
+        self._corpus["parsed_text"].apply(
             lambda text: vocabulary.update(text.split(" ")))
         vocabulary.discard("")
         return vocabulary
 
-    def compute_tf(self) -> pd.DataFrame:
+    def _compute_tf(self) -> pd.DataFrame:
         """
-        Given a Pandas DataFrame with a column "parsed_text" and a set of unique words in the corpus,
-        returns a DataFrame with a column for each unique word in the corpus, containing the number of times
-        that word appears in the document.
+        Create a dataframe with the term frequency of each term in the vocabulary.
 
         params:
             df: Pandas DataFrame with a column "parsed_text"
@@ -72,33 +70,32 @@ class BM25(object):
                                that word appears in the document.
         """
         term_frequency = pd.DataFrame(
-            index=self.corpus["ids"], columns=self.vocabulary)
-        for _, row in self.corpus.iterrows():
+            index=self._corpus["id"], columns=self._vocabulary)
+        for _, row in self._corpus.iterrows():
             tmp = row["parsed_text"].split(" ")
             for term in set(tmp) - {""}:
-                term_frequency.loc[row["ids"], term] = tmp.count(term)
+                term_frequency.loc[row["id"], term] = tmp.count(term)
         return term_frequency.fillna(0)
 
-    @staticmethod
-    def compute_idf(tf: pd.DataFrame, vocabulary: Set[str], n_docs: int) -> pd.DataFrame:
+    def _compute_idf(self) -> pd.DataFrame:
         """
-        Given a Pandas DataFrame with a column for each unique word in the corpus, containing the number of times
-        that word appears in the document, and a set of unique words in the corpus, returns a DataFrame with a row
-        for each unique word in the corpus, containing the inverse document frequency of that word.
+        Compute the inverse document frequency of each term in the vocabulary,
+        using the formula idf = log(N / df) + 1.
 
-        $idf = log(N / (df + 1))$
-
+        returns:
+            idf (pd.DataFrame): DataFrame with a column for each unique word in the corpus, containing the inverse document
+                                    frequency of that word.
         """
-        vocabulary = list(vocabulary)
+        vocabulary = list(self._vocabulary)
         idf = pd.DataFrame(index=vocabulary)
         idf["word"] = vocabulary
         idf["idf"] = idf["word"].apply(
-            lambda word: math.log(n_docs / (tf[word].sum() + 1)))
+            lambda word: math.log(len(self._corpus) / (self._tf[word].sum() + 1)))
         idf.drop(columns="word", inplace=True)
         return idf
 
     @staticmethod
-    def preprocess_text(text: str) -> str:
+    def _preprocess_text(text: str) -> str:
         """
         Preprocess each string by:
         1. Lowercasing the string
@@ -114,42 +111,39 @@ class BM25(object):
                         if word not in STOP_WORDS])
         return text
 
-    def score_document(self, corpus_row: pd.DataFrame, query_terms: Set[str]) -> float:
+    def _score_document(self, corpus_row: pd.DataFrame, query_terms: Set[str]) -> float:
+        """
+        Compute the BM25 score for a document, given a set of query terms.
+        """
         score = 0
         for term in query_terms:
-            if term in self.vocabulary:
-                temp = self.score_term(corpus_row, term)
+            if term in self._vocabulary:
+                temp = self._score_term(corpus_row, term)
                 score += temp
         return round(score, 4)
 
-    def score_term(self, corpus_row: pd.DataFrame, term: str) -> float:
+    def _score_term(self, corpus_row: pd.DataFrame, term: str) -> float:
         """
         Given a corpus row and a term, returns the BM25 score for that term.
-
-        params:
-            corpus_row: corpus row
-            term: term
-
-        returns:
-            score (float): BM25 score for that term
         """
-        tf = self.tf.loc[corpus_row["ids"], term]
-        term_idf = self.idf.loc[term, "idf"]
-        return (self.k + 1) * tf * term_idf / ((self.k + tf) * (1 - self.b + self.b * corpus_row["word_count"] / self.avg_word_count))
+        tf = self._tf.loc[corpus_row["id"], term]
+        term_idf = self._idf.loc[term, "idf"]
+        return ((self.k1 + 1) * tf + self.delta) * term_idf / ((self.k1 + tf) * (1 - self.b + self.b * corpus_row["word_count"] / self._avg_word_count))
 
+    @lru_cache
     def query(self, query: str, max_results: int, expand=False) -> pd.DataFrame:
         """
 
         """
-        query = self.preprocess_text(query)
+        query = self._preprocess_text(query)
         query_terms = set(query.split(" "))
-        results = self.corpus.copy().drop(columns="word_count")
-        results["score"] = self.corpus.apply(
-            lambda row: self.score_document(row, query_terms), axis=1)
+        results = self._corpus.copy().drop(columns="word_count")
+        results["score"] = self._corpus.apply(
+            lambda row: self._score_document(row, query_terms), axis=1)
         results = results[results["score"] > 0]
         results = results.sort_values(by="score", ascending=False)
         if expand:
-            rf_tf = self.tf.loc[results[:min(self.rf_docs, len(results))]["ids"], :].sum(
+            rf_tf = self._tf.loc[results[:min(self.rf_docs, len(results))]["id"], :].sum(
             ).transpose().sort_values(ascending=False)[:self.rf_terms]
             query_terms = query_terms.union(
                 set(rf_tf.loc[rf_tf > 0].index.to_list()))
--- demo.py
+++ demo.py
@@ -1,36 +1,67 @@
 import os
-from numpy import exp2
+import time
+import argparse
 import pandas as pd
 from nltk import corpus
 from bm25 import BM25
 from tabulate import tabulate
 
 
+def get_args():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--ndocs', type=int, default=2500,
+                        help='Number of documents to load from the corpus')
+    parser.add_argument('--k1', type=float, default=1.2,
+                        help='Coefficient in the BM25 formula')
+    parser.add_argument('--b', type=float, default=0.75,
+                        help='Coefficient in the BM25 formula')
+    parser.add_argument('--delta', type=float, default=0.0,
+                        help='Coefficient in the BM25+ formula')
+    parser.add_argument('--rf_docs', type=int, default=15,
+                        help='Number of documents to use for pseudo-relevance feedback')
+    parser.add_argument('--rf_terms', type=int, default=20,
+                        help='Number of terms to use for pseudo-relevance feedback')
+    parser.add_argument('--nresults', type=int, default=25,
+                        help='Number of documents to return')
+    parser.add_argument('--expand', type=bool, default=False,
+                        help='Expand the query with pseudo-relevance feedback')
+    args = parser.parse_args()
+    return args
+
+
 def extract_title(text: str) -> str:
-    words = text.split(" ")
     i = 0
-    while i < len(words) and words[i].upper() == words[i]:
+    while i < len(text) and text[i].upper() == text[i]:
         i += 1
-    return " ".join(words[:i])
+    return text[:i]
 
 
 if __name__ == "__main__":
 
+    args = get_args()
+
+    # Load the corpus
     file_ids = corpus.reuters.fileids()
-    all_reuters_words = []
-    for file_id in file_ids:
-        file_words = corpus.reuters.words(file_id)
-        output = " ".join(file_words)
-        all_reuters_words.append([file_id, output])
-    df_reuters = pd.DataFrame(all_reuters_words, columns=["ids", "text"])
-    df_reuters = df_reuters[:2500]
-    df_reuters["title"] = df_reuters["text"].apply(extract_title)
+    data = []
+    for i in range(min(args.ndocs, len(file_ids))):
+        words = corpus.reuters.words(file_ids[i])
+        text = " ".join(words)
+        title = extract_title(text)
+        data.append([file_ids[i], title, text])
+    df = pd.DataFrame(data, columns=['id', 'title', 'text'])
 
-    bm25 = BM25(df_reuters, 1.25, 0.75, 1, rf_docs=15)
+    # Create a BM25 index
+    bm25 = BM25(df, args.k1, args.b, args.delta, args.rf_docs, args.rf_terms)
 
+    # Perform queries
     while True:
         os.system('clear')
         query = input("Enter a plain-text query: ")
-        results = bm25.query(query, 45, expand=True)
-        print(tabulate(results, headers=["ids", "title", "score"]))
-        input("Press Enter to continue...")
+        start = time.time()
+        results = bm25.query(query, args.nresults, expand=args.expand)
+        end = time.time()
+        print("\nQuery time: %.2f seconds\n" % (end - start))
+        print(tabulate(results, headers=[
+              "id", "title", "score"], showindex=False))
+        if input("\nPress enter to continue or q to quit: ") == "q":
+            break
