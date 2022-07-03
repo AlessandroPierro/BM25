7952273a9b38ff370fff0aa283836c93ba0987dd
Alessandro Pierro <40338642+AlessandroPierro@users.noreply.github.com>
2022-07-03 17:13:15 +0200

    Add score plotting

--- demo.py
+++ demo.py
@@ -7,9 +7,11 @@ from nltk import corpus
 from bm25 import BM25
 from tabulate import tabulate
 
+pd.options.plotting.backend = "plotly"
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser = argparse.ArgumentParser(description='Information Retrieval system based on BM25.\n'
+                                                    'This program is a demo of the BM25 algorithm.')
     parser.add_argument('--ndocs', type=int, default=2500,
                         help='Number of documents to load from the corpus')
     parser.add_argument('--k1', type=float, default=1.2,
@@ -20,9 +22,9 @@ def get_args():
                         help='Coefficient in the BM25+ formula')
     parser.add_argument('--rf_docs', type=int, default=15,
                         help='Number of documents to use for pseudo-relevance feedback')
-    parser.add_argument('--rf_terms', type=int, default=20,
+    parser.add_argument('--rf_terms', type=int, default=10,
                         help='Number of terms to use for pseudo-relevance feedback')
-    parser.add_argument('--nresults', type=int, default=25,
+    parser.add_argument('--nresults', type=int, default=30,
                         help='Number of documents to return')
     parser.add_argument('--expand', type=bool, default=False,
                         help='Expand the query with pseudo-relevance feedback')
@@ -30,6 +32,8 @@ def get_args():
                         help='Path to a pickle file containing a previously-saved BM25 object')
     parser.add_argument('--dump', type=str, default=None,
                         help='Path to a pickle file to save the BM25 object')
+    parser.add_argument('--plot', type=bool, default=False,
+                        help='Plot the score distribution')
     args = parser.parse_args()
     return args
 
@@ -38,6 +42,7 @@ def extract_title(text: str) -> str:
     i = 0
     while i < len(text) and text[i].upper() == text[i]:
         i += 1
+    i = min(i, 40)
     return text[:i]
 
 
@@ -74,5 +79,14 @@ if __name__ == "__main__":
         print("\nQuery time: %.2f seconds\n" % (end - start))
         print(tabulate(results, headers=[
             "id", "title", "score"], showindex=False))
+        if args.plot:
+            results.drop(['id', 'title'], axis=1, inplace=True)
+            results.index = list(range(len(results)))
+            fig = results.plot.bar()
+            fig.update_layout(legend_title_text="Legend")
+            fig.update_xaxes(title_text="Result Index")
+            fig.update_yaxes(title_text="Score")
+            fig.update_layout(title_text=f"Query Results for '{query}'")
+            fig.show()
         if input("\nPress enter to continue or q to quit: ") == "q":
             break
